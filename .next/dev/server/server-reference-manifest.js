3420fcb4cfa4e275825dc36e599791b40ef95bff
Suazo-Barber <suazo@suazobarber.com>
2025-11-07 22:18:35 +0100

    feat: initial clean client (sin .next / node_modules)

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ttdJEScNBSRaCpHHxpllqxfoj602R1WPrlDtmWCtmHE=\"\n}"
---
+++ .next/dev/static/development/_buildManifest.js
@@ -0,0 +1,21 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/admin",
+    "/admin/relatorios",
+    "/agendar",
+    "/barber",
+    "/contato",
+    "/galeria",
+    "/login",
+    "/produtos",
+    "/servicos"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
